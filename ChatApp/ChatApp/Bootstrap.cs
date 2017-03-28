e7a01e2617aa07633c41ccca50f634181551467a
Erik Morrow <cyborgpencil@gmail.com>
2017-03-27 18:57:03 -0600

    Prism setup

---
+++ ChatApp/ChatApp/Bootstrap.cs
@@ -0,0 +1,33 @@
+﻿using ChatApp.Views;
+using Microsoft.Practices.ServiceLocation;
+using Prism;
+using Prism.Modularity;
+using Prism.Unity;
+using System;
+using System.Windows;
+
+namespace ChatApp
+{
+    public class Bootstrap : UnityBootstrapper
+    {
+        public MainWindow MainWindow { get; set; }
+
+        protected override DependencyObject CreateShell()
+        {
+            MainWindow = ServiceLocator.Current.GetInstance<MainWindow>();
+            return MainWindow;
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow = MainWindow;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return base.CreateModuleCatalog();
+        }
+
+    }
+}
